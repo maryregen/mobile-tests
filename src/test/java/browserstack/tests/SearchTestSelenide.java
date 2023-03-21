e2578f4074e5d10b71dd5752821ce61badf76628
maryregen <maryregen@yandex.ru>
2023-03-21 19:32:32 +0400

    Changed ignored files

--- src/test/java/browserstack/tests/SearchTestSelenide.java
+++ src/test/java/browserstack/tests/SearchTestSelenide.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.CollectionCondition.sizeGreaterThan;
 import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$$;
 import static io.appium.java_client.AppiumBy.accessibilityId;
